735bcdcff70adc3aad2f7b3e0dbcec387846e0aa
Pierre-Yves Landuré <pierre-yves@landure.fr>
2025-10-08 08:43:51 +0200

    chore(devenv): :technologist: initialize devenv

---
+++ devenv.nix
@@ -0,0 +1,49 @@
+{
+  inputs,
+  ...
+}:
+{
+  # https://devenv.sh/basics/
+  name = "Taskfile (go-task) devenv module";
+
+  imports = [
+    "${inputs.devenv-recipes}/devenv-scripts.nix"
+    "${inputs.devenv-recipes}/git.nix"
+    "${inputs.devenv-recipes}/markdown"
+    "${inputs.devenv-recipes}/nix"
+    "${inputs.devenv-recipes}/secrets"
+  ];
+
+  # https://devenv.sh/packages/
+  # packages = [ ];
+
+  # https://devenv.sh/languages/
+  # languages.rust.enable = true;
+
+  # https://devenv.sh/processes/
+  # processes.dev.exec = "${lib.getExe pkgs.watchexec} -n -- ls -la";
+
+  # https://devenv.sh/services/
+  # services.postgres.enable = true;
+
+  # https://devenv.sh/scripts/
+
+  # https://devenv.sh/basics/
+  # enterShell = '''';
+
+  # https://devenv.sh/tasks/
+  # tasks = {
+  #   "myproj:setup".exec = "mytool build";
+  #   "devenv:enterShell".after = [ "myproj:setup" ];
+  # };
+
+  # https://devenv.sh/tests/
+  # enterTest = ''
+  #  echo "Running tests"
+  #'';
+
+  # https://devenv.sh/git-hooks/
+  # git-hooks.hooks.shellcheck.enable = true;
+
+  # See full reference at https://devenv.sh/reference/options/
+}
